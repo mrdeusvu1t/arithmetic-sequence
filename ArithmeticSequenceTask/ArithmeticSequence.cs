49ac661b92a6607354ade2c23dacd35bc7ea38b6
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-04-23 15:01:50 +0300

    add-task-and-test-projects

---
+++ ArithmeticSequenceTask/ArithmeticSequence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArithmeticSequenceTask
+{
+    public static class ArithmeticSequence
+    {
+        public static int Calculate(int number, int add, int count)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
