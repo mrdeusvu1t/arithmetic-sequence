64f38fd9911643e3acfcf6ffa0b3f043bcf89696
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-28 17:54:02 +0000

    Replace ArithmeticSequence.cs

--- ArithmeticSequenceTask/ArithmeticSequence.cs
+++ ArithmeticSequenceTask/ArithmeticSequence.cs
@@ -4,6 +4,20 @@ namespace ArithmeticSequenceTask
 {
     public static class ArithmeticSequence
     {
+        /// <summary>
+        /// Calculates the sum of the first 'count' elements of a sequence in which each element is the sum of the given integer 'number'
+        /// and number of occurrences of the given integer 'add', based on the element's position within the sequence.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <param name="add">The term.</param>
+        /// <param name="count">The number of occurrences.</param>
+        /// <returns>Calculated sum.</returns>
+        /// <exception cref="OverflowException">
+        /// Throw if number is int.MaxValue and term more then zero
+        /// - or
+        /// number is int.MinValue and term less then zero.
+        /// </exception>
+        /// <exception cref="ArgumentException">Throw if count less then zero.</exception>
         public static int Calculate(int number, int add, int count)
         {
             throw new NotImplementedException("You need to implement this function.");
